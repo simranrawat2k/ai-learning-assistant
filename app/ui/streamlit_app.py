3af7127fb8ccf7b9526d1f27683c1cb9a78c3542
Simran Rawat <simranrawat2k@gmail.com>
2026-06-20 19:27:16 +0530

    Completed PDF RAG chatbot

--- app/ui/streamlit_app.py
+++ app/ui/streamlit_app.py
@@ -1,5 +1,13 @@
-import streamlit as st
+import sys
 import os
+
+sys.path.append(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "../..")
+    )
+)
+
+import streamlit as st
 import hashlib
 import tempfile
 
